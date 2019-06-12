c5316d04ddb6fa2a50b59bc98c5548d501661abc
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2019-06-12 13:48:04 -0400

    Update CPT: Links for Images.py

--- CPT: Links for Images.py
+++ CPT: Links for Images.py
@@ -1,13 +1,21 @@
 '''Links'''
 
 #Menu Background
+'''
 https://www.google.com/search?rlz=1C1CHBF_enCA751CA751&tbm=isch&q=basketball+court+background&chips=q:basketball+court+background,g_1:vector:zqiJIiip3Vk%3D&usg=AI4_-kTKwIRnHbcR6GJiE224dEBQOxJvrA&sa=X&ved=0ahUKEwiR0riBgsLiAhVUMH0KHc5SBrEQ4lYIMygG&biw=1440&bih=799&dpr=1#imgrc=3r4GO6wjwEy16M:
+'''
 
 #Net
+'''
 https://www.google.com/search?rlz=1C1CHBF_enCA751CA751&tbm=isch&q=basketball+net&chips=q:basketball+net,g_1:vector:50OZQ3uvJHw%3D&usg=AI4_-kS1_hlXAjjnrydiUd9Tb3uiolR7sA&sa=X&ved=0ahUKEwiqy-35wMjiAhVRlKwKHaDlDnAQ4lYIMCgD&biw=1440&bih=799&dpr=1#imgdii=40HXvioW8G3vhM:&imgrc=MD1POj9tE9hSsM:
+'''
 
 #Ball
+'''
 https://www.google.com/search?rlz=1C1CHBF_enCA751CA751&biw=1440&bih=750&tbm=isch&sa=1&ei=5pDyXLegDouytQXA_pnQDg&q=basketball+vector&oq=basketball+ve&gs_l=img.1.0.0l10.5121.6677..7934...0.0..0.113.288.2j1......0....1..gws-wiz-img.......35i39j0i67.iUjMKKlNgxI#imgdii=l4wLqMZ7G6BA_M:&imgrc=uJbQPM98H8K5FM:
+'''
 
 # Health Power-up
+'''
 https://www.google.com/search?biw=1366&bih=666&tbm=isch&sa=1&ei=Gq_2XIGPIcyGsAWHmKXgCg&q=plus+sign+png&oq=plus+sign&gs_l=img.1.1.0l10.30273.34659..37284...0.0..0.348.2106.0j4j3j2......0....1..gws-wiz-img.....0..0i67.6Qz5Kdde7yc#imgrc=Tw-AarnO8oeFLM:
+'''
